9f9a64cb5dcd8e1564a657932db1d29c536939dd
CaroReyes82 <carolina.brj@gmail.com>
2020-09-02 10:38:01 -0400

    nuevas operaciones

--- calculadora.js
+++ calculadora.js
@@ -97,22 +97,24 @@ function resetear(){
 function resolver(){
 	var res = 0;
 	switch(operacion){
-		case "+":
-			res = parseFloat(operandoa) + parseFloat(operandob);
+		case "^2":
+			res = Math.pow(parseFloat(operandoa),2);
 			break;
-
-		case "-":
-		    res = parseFloat(operandoa) - parseFloat(operandob);
-		    break;
-
-		case "*":
-			res = parseFloat(operandoa) * parseFloat(operandob);
+		case "√":
+			res = Math.sqrt(parseFloat(operandoa));
 			break;
-
-		case "/":
-			res = parseFloat(operandoa) / parseFloat(operandob);
+		case "Log":
+			res = Math.log(parseFloat(operandoa));
 			break;
+		case "Exp":
+			res = Math.exp(parseFloat(operandoa));
+			break;
+		
 	}
 	resetear();
 	resultado.textContent = res;
-}
+
+
+
+}	
+
